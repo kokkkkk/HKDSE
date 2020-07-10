6418996f30df6527d2a494e4a4e7ae9b50540a3f
kokkkkk <ko.kiki12355@gmail.com>
2020-07-10 22:04:40 +0800

    add background to the title

--- src/frame/title.java
+++ src/frame/title.java
@@ -5,7 +5,13 @@ import main.saveLoad;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,9 +29,11 @@ public class title{
 	Font smallFont = new Font("Times New Roman", Font.PLAIN,17);
 	Font VerysmallFont = new Font("Times New Roman", Font.PLAIN,9);
 	
-	JPanel title, choices;
-	JLabel titleName;
+	JPanel title, choices, titleImagePanel;
+	JLabel titleName,titleImageLabel;
 	JButton newGameButton,continueButton,exitButton;
+	
+	ImageIcon titleImage,newGameImage;
 
 	main.saveLoad saveLoad = new saveLoad();
 	
@@ -42,20 +50,26 @@ public class title{
 		window.setLayout(null); //customize layout not use default layout
 		window.setFocusable(true);
 		
-		//con = window.getContentPane();
-		
 		con = window.getLayeredPane();
+		
+		titleImage = new ImageIcon(getClass().getClassLoader().getResource("background.png"));
+		
+		titleImagePanel = new JPanel();
+		titleImagePanel.setBounds(0,0,800,600);
+		
+		titleImageLabel = new JLabel();
+		titleImageLabel.setIcon(titleImage);
 		
 		titleHandler = new Control.titleHandler(this);
 		
 		title = new JPanel();
 		title.setBounds(100,30,600,100);
-		title.setBackground(Color.RED);
+		title.setOpaque(false);
 		
 		choices = new JPanel();
 		choices.setLayout(new GridLayout(3,1));
 		choices.setBounds(160,160,500,300);
-		choices.setBackground(Color.blue);
+		choices.setOpaque(false);
 		
 		titleName = new JLabel("DSE Life");
 		titleName.setFont(titleFont);
@@ -65,15 +79,19 @@ public class title{
 		newGameButton.setBackground(Color.black);
 		newGameButton.setForeground(Color.white);
 		newGameButton.setFont(normalFont);
+		newGameButton.setMargin(new Insets(0,0,0,0));
+		newGameButton.setContentAreaFilled(false);
 		newGameButton.setFocusPainted(false);
 		newGameButton.setActionCommand("new");
 		newGameButton.addActionListener(titleHandler);
+
 		
 		continueButton = new JButton("Continue");
 		continueButton.setBackground(Color.black);
 		continueButton.setForeground(Color.white);
 		continueButton.setFont(normalFont);
 		continueButton.setFocusPainted(false);
+		continueButton.setContentAreaFilled(false);
 		continueButton.setActionCommand("continue");
 		continueButton.addActionListener(titleHandler);
 		
@@ -82,9 +100,11 @@ public class title{
 		exitButton.setForeground(Color.white);
 		exitButton.setFont(normalFont);
 		exitButton.setFocusPainted(false);
+		exitButton.setContentAreaFilled(false);
 		exitButton.setActionCommand("exit");
 		exitButton.addActionListener(titleHandler);
 		
+		titleImagePanel.add(titleImageLabel);
 		title.add(titleName);
 		choices.add(newGameButton);
 		choices.add(continueButton);
@@ -104,11 +124,23 @@ public class title{
 		
 		con.add(title);
 		con.add(choices);
+		con.add(titleImagePanel);
+		
+		/*
+		BufferedImage image = null;
+		try{
+			image = ImageIO.read(getClass().getClassLoader().getResource("BigButtonRegular.png"));
+		}catch(IOException e){
+			
+		}
+		Image img = image.getScaledInstance(choices.getWidth(), 100, Image.SCALE_SMOOTH);
+		newGameImage = new ImageIcon(img);
+		newGameButton.setIcon(newGameImage);
 		
 		if(!saveLoad.checkFileExist()){
 			continueButton.setEnabled(false);
 		}
-		
+		*/
 		title.setVisible(true);
 		choices.setVisible(true);
 		
@@ -118,6 +150,6 @@ public class title{
 		
 		title.setVisible(false);
 		choices.setVisible(false);
-		
+		titleImagePanel.setVisible(false);
 	}
 }
