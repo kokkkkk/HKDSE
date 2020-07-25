507ccb0e36d0111630f74cca43d7f5264f407d42
kokkkkk <ko.kiki12355@gmail.com>
2020-07-25 20:06:33 +0800

    add buttonClick sound effect

--- src/Basic/o_jbutton.java
+++ src/Basic/o_jbutton.java
@@ -3,6 +3,8 @@ package Basic;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
@@ -12,6 +14,7 @@ public class o_jbutton extends JButton{
 	private static final long serialVersionUID = -1111560679427958283L;
 
 	Basic.addUI addUI = new Basic.addUI();
+	Basic.soundEffect se = new Basic.soundEffect();
 	
 	boolean initial = false;
 	
@@ -33,6 +36,15 @@ public class o_jbutton extends JButton{
 			this.setContentAreaFilled(false);
 			this.setHorizontalAlignment(SwingConstants.LEFT);
 			this.setHorizontalTextPosition(JButton.CENTER);
+			
+			this.addActionListener(new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent e){
+					se.setFile("buttonClick.wav");
+					se.play();
+				}
+			});
+			
 			initial = true;
 		}
 	
---
+++ src/Basic/soundEffect.java
@@ -0,0 +1,35 @@
+package Basic;
+
+import java.io.File;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class soundEffect {
+
+	Clip clip;
+	
+	public void setFile(String fileName){
+		
+		try{
+			
+			URL url = this.getClass().getClassLoader().getResource(fileName);
+			AudioInputStream sound = AudioSystem.getAudioInputStream(url);
+			clip = AudioSystem.getClip();
+			clip.open(sound);
+			
+		}catch(Exception e){
+			
+		}
+		
+	}
+	
+	public void play(){
+		
+		clip.setFramePosition(0);
+		clip.start();
+	}
+	
+}
